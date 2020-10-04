e1f8c80c34fefd31bc9375a7e7c375335a68d513
Abhishek Bhavsar <abhiraj2k@gmail.com>
2020-10-04 19:59:18 +0530

    Updates

---
+++ precache-manifest.334092e7adae58786238631199dd134f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11afaece07b286794c2de1a079534148",
+    "url": "/VideoSearch/index.html"
+  },
+  {
+    "revision": "e87578b41881769ecd1b",
+    "url": "/VideoSearch/static/css/main.1f997e6c.chunk.css"
+  },
+  {
+    "revision": "6abe413f2308391f0a71",
+    "url": "/VideoSearch/static/js/2.14c150f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/VideoSearch/static/js/2.14c150f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e87578b41881769ecd1b",
+    "url": "/VideoSearch/static/js/main.4e359e5e.chunk.js"
+  },
+  {
+    "revision": "2a0f216d0088c2200efe",
+    "url": "/VideoSearch/static/js/runtime-main.55d5dc0e.js"
+  },
+  {
+    "revision": "70883052bbc4ff036254b2ecdb16bf1b",
+    "url": "/VideoSearch/static/media/chandana-ban-5gdnxHjCaFY-unsplash.70883052.jpg"
+  }
+]);
